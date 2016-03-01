8f820bda3f2f4346a2eda9160c3aa9ffac8d33fc
Alex Blair <ablair18@my.whitworth.edu>
2016-02-29 20:08:32 -0800

    Implement Beginning Activity

--- First_Project/MyFirstApp/app/src/main/java/com/example/alex/myfirstapp/MyActivity.java
+++ First_Project/MyFirstApp/app/src/main/java/com/example/alex/myfirstapp/MyActivity.java
@@ -1,16 +1,20 @@
 package com.example.alex.myfirstapp;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.example.alex.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +51,20 @@ public class MyActivity extends AppCompatActivity {
             return true;
         }
 
+
         return super.onOptionsItemSelected(item);
     }
-}
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+// Do something in response to button press
+
+    }
+
+    }
+
+
